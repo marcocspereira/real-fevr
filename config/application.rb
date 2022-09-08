8349fa1b7e82216ad023a7e6fc16254e299249de
Marco Pereira <marcocsp@hotmail.com>
2022-09-08 10:06:40 +0100

    Enable uuid instead id as primary key

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      # This automatically adds id: :uuid to create_table in all future migrations.
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
