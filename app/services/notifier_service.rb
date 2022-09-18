9de41e5ac727a847908523ff9b5970bce77fea4e
Marco Pereira <marcocsp@hotmail.com>
2022-09-18 09:03:32 +0100

    Remove unecessary puts once again

--- app/services/notifier_service.rb
+++ app/services/notifier_service.rb
@@ -1,7 +1,6 @@
 class NotifierService
 
   def self.publish(resource, message)
-    puts "#######"
     subscribers_ids = PlayerSubscription.player_subscribers_ids(resource.id)
     subscribers_emails = User.where(id: subscribers_ids).pluck(:email)
     
