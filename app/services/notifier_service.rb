ec91d23d8d5b23d26223a5a20dbd3bd93c78b41e
Marco Pereira <marcocsp@hotmail.com>
2022-09-18 09:03:01 +0100

    Remove unecessary puts

--- app/services/notifier_service.rb
+++ app/services/notifier_service.rb
@@ -4,14 +4,8 @@ class NotifierService
     puts "#######"
     subscribers_ids = PlayerSubscription.player_subscribers_ids(resource.id)
     subscribers_emails = User.where(id: subscribers_ids).pluck(:email)
-    puts subscribers_emails
-    # enviar email
-    puts "NotifierService: #{resource.id}"
-    
-    puts subscribers_emails
     
     subscribers_emails.each do |email|
-      puts "vou chamar o mailer para #{email} com a messagem #{message}. O jogador #{resource.name}"
       mail = NotificationMailer.with(recipient: email, message: message, resource: resource)
                                .player_notification_email
       mail.deliver_now!
